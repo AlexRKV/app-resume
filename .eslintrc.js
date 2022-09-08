0d03f9cca9fd668c08e122fa6acb5d39f3c365b7
Ваше Имя <you@example.com>
2022-09-08 16:11:36 +0300

    init: start-resume

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,28 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/airbnb',
-  ],
-  parserOptions: {
-    parser: '@babel/eslint-parser',
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  },
+    root: true,
+    env: {
+        node: true,
+    },
+    extends: [
+        'plugin:vue/vue3-essential',
+        '@vue/airbnb',
+    ],
+    parserOptions: {
+        parser: '@babel/eslint-parser',
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        "quotes": [0, "single", { "avoidEscape": true }],
+        'indent': 'off', // отступы
+        "eol-last": 0,
+        "no-multiple-empty-lines": ["error", { "max": 1, "maxEOF": 0 }],
+        "vuejs-accessibility/label-has-for": ["error", {
+            "required": {
+                "some": ["nesting", "id"]
+            }
+        }],
+        'comma-dangle': [0, "always-multiline"],
+        "arrow-body-style": "off"
+    },
 };
