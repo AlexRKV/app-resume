5a5018cbf8e9f00296da02fdf7d673e4daefa845
Alexander <multi5@rambler.ru>
2022-09-11 15:20:58 +0300

    init: vue-mixin

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,7 @@
-<template>
-    <div class="container column">
-        <AppSettingResume @addBlock="addBlock1" />
-        <AppViewResumeBlock :blocks="blocks" />
-    </div>
-    <div class="container">
-        <AppLoaderBlock v-if="loader" />
-        <AppCommentsBlock
-            v-else
-            @loadComments="addLoadComments"
-            :comments="comments"
-        />
-    </div>
-</template>
+<template></template>
 
 <script>
-import AppSettingResume from "@/components/AppSettingResume.vue";
-import AppViewResumeBlock from "@/components/AppViewResumeBlock.vue";
-import AppCommentsBlock from "@/components/AppCommentsBlock.vue";
-import AppLoaderBlock from "@/components/AppLoaderBlock.vue";
-// import axios from "axios";
-
-export default {
-    components: {
-        AppSettingResume,
-        AppViewResumeBlock,
-        AppCommentsBlock,
-        AppLoaderBlock,
-    },
-    data() {
-        return {
-            blocks: [],
-            comments: [],
-            loader: false,
-        };
-    },
-    methods: {
-        addBlock1(blockValue) {
-            this.blocks.push(blockValue);
-        },
-        // async addLoadComments() {
-        //     this.loader = true;
-        //     const { data } = await axios.get(
-        //         "https://jsonplaceholder.typicode.com/comments?_limit=22"
-        //     );
-        //     this.loader = false;
-        //     this.comments = data;
-        //     console.log(data);
-        // },
-        async addLoadComments() {
-            this.loader = true;
-            const response = await fetch(
-                "https://jsonplaceholder.typicode.com/comments?_limit=22"
-            );
-            this.loader = false;
-            this.comments = await response.json();
-        },
-    },
-};
+export default {};
 </script>
 
 <style>
